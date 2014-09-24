b3940dbf4df770d2d7bf822c06b5ceed4a48074d
Travis Reed <travis.reed@webfilings.com>
2014-09-24 17:52:28 -0500

    Break stuff

--- src/models.py
+++ src/models.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(db.ModelTravis):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(128))

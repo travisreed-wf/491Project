357740cce233b6d96a98386f08e1088c0cf448c2
Travis Reed <travis.reed@webfilings.com>
2014-10-19 20:23:55 -0500

    quizBuilder is 100% done

--- src/questionBuilder/helper_functions.py
+++ src/questionBuilder/helper_functions.py
@@ -2,7 +2,7 @@ import resources
 
 
 def get_elements():
-    multipleChoice = resources.Element("Multiple Choice", "elements/multipleChoice.html")
+    multipleChoice = resources.Element("Multiple Choice", "elements/multipleChoice")
     freeResponse = resources.Element("Free Response", "elements/freeResponse.html")
     trueFalse = resources.Element("True/False", "elements/trueFalse.html")
     video = resources.Element("Video", "elements/video.html")
--- src/questionBuilder/views.py
+++ src/questionBuilder/views.py
@@ -19,3 +19,9 @@ class QuizBuilderView(MethodView):
     def get(self):
         elements = helper_functions.get_elements()
         return render_template("quizBuilder.html", elements=elements)
+
+
+class MultipleChoiceView(MethodView):
+
+    def get(self):
+        return render_template("elements/multipleChoice.html")

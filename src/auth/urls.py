55d2d3713722bf8f2fd9e0b6cddc15d222e32346
jdmielke <jdmielke@iastate.edu>
2014-10-29 20:05:45 -0500

    Merge branch 'master' of https://github.com/jdmielke/491Project
    Conflicts:
    src/models.py

--- src/auth/urls.py
+++ src/auth/urls.py
@@ -4,3 +4,4 @@ import views
 def setup_urls(app):
     app.add_url_rule('/login', view_func=views.LoginView.as_view('login'))
     app.add_url_rule('/logout', view_func=views.LogoutView.as_view('logout'))
+    app.add_url_rule('/register', view_func=views.RegisterView.as_view('register'))
--- src/home/views.py
+++ src/home/views.py
@@ -5,10 +5,23 @@ from flask import render_template
 from flask import url_for
 from flask.views import MethodView
 from flask_login import login_required
+import models
+import re
+import json
 
 
 class HomeScreenView(MethodView):
     decorators = [login_required]
 
     def get(self):
-        return render_template('home.html', text="")
+        return render_template('home.html')
+
+class DBButtonView(MethodView):
+    def get(self):
+        #user = models.User("test@test.com", "pass")
+        #user = models.db.session.query(models.User).filter_by(id=1).first()
+        #user.courses.append(models.db.session.query(models.Course).filter_by(id=1).first())
+        #models.db.session.commit()
+        #print user.courses[0].name
+        #print models.db.session.query(models.Course).filter_by(id=1).first()
+        return render_template('home.html')

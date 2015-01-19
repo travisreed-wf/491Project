4b06cb825099da87e61f4de1a4a3dfe8f10847ce
Travis Reed <travis.reed@webfilings.com>
2015-01-19 16:00:56 -0600

    image uploads

--- src/taskBuilder/views.py
+++ src/taskBuilder/views.py
@@ -1,16 +1,62 @@
-import re
-
 import flask
 from flask import redirect
 from flask import render_template
 from flask import url_for
 from flask.views import MethodView
 from flask_login import login_required
+from werkzeug import secure_filename
+
+import os
+import json
+import re
 
 import auth
 from auth import auth
+import config
 import helper_functions
 import models
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in config.ALLOWED_EXTENSIONS
+
+
+def allowed_image(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in config.ALLOWED_IMAGE_EXTENSIONS
+
+
+def get_thumbnail(extension):
+    if extension == "pdf":
+        return models.Thumbnail.query.filter_by(id=1).first()
+    elif extension == "txt":
+        return models.Thumbnail.query.filter_by(id=2).first()
+    elif extension == "mp3":
+        return models.Thumbnail.query.filter_by(id=4).first()
+    else:
+        return models.Thumbnail.query.filter_by(id=3).first()
+
+
+class UploadView(MethodView):
+
+    def get(self):
+        return render_template('upload.html')
+
+    def post(self):
+        f = flask.request.files.get('file_path')
+        print f
+        print f.filename
+        if f and allowed_file(f.filename):
+            f.save(os.path.join("src/static/uploads", f.filename))
+            return "Success"
+        return "Failed"
+
+
+class UploadedFileView(MethodView):
+
+    def get(self, filename):
+        return flask.send_file("static/uploads/" + filename)
 
 
 class TaskBuilderView(MethodView):

09756fd5242e13190916367f6f0fe01d01754f62
Andrew Guibert <andy.guibert@gmail.com>
2015-01-25 20:02:53 -0600

    Images for task builder

--- src/taskBuilder/helper_functions.py
+++ src/taskBuilder/helper_functions.py
@@ -2,9 +2,9 @@ import resources
 
 
 def get_elements():
-    multipleChoice = resources.Element("Multiple Choice", "elements/multipleChoice")
-    freeResponse = resources.Element("Free Response", "elements/freeResponse")
-    trueFalse = resources.Element("True/False", "elements/trueFalse")
-    supplementary = resources.Element("Supp. Material", "elements/supplementary")
+    multipleChoice = resources.Element("Multiple Choice", "elements/multipleChoice", "../static/images/multiple-choice.png")
+    freeResponse = resources.Element("Free Response", "elements/freeResponse", "../static/images/free-response.png")
+    trueFalse = resources.Element("True/False", "elements/trueFalse", "../static/images/true-false.png")
+    supplementary = resources.Element("Supp. Material", "elements/supplementary", "../static/images/video-button.png")
     elements = [multipleChoice, freeResponse, trueFalse, supplementary]
     return elements

3827e56ddbff5290b28bfd443e210c68eaeb264a
Travis Reed <travis.reed@webfilings.com>
2015-03-08 22:52:37 -0500

    do gradebook on page load

--- src/gradebook/views.py
+++ src/gradebook/views.py
@@ -5,11 +5,34 @@ from flask import render_template
 from flask import url_for
 from flask.views import MethodView
 from flask_login import login_required
+from flask_login import current_user
+
+import models
 
 
-class gradebookScreenView(MethodView):
+class GradebookScreenView(MethodView):
     decorators = [login_required]
 
     def get(self):
-        text = flask.request.args.get('text', "")
-        return render_template('gradebook.html', text=text)
+        data = []
+        for c in current_user.courses:
+            tasks = []
+            d = {
+                'course': c,
+            }
+            for task in c.tasks:
+                response = models.TaskResponse.query.filter(
+                    models.TaskResponse.student_id==current_user.id,
+                    models.TaskResponse.task_id==task.id).order_by(
+                    models.TaskResponse.datetime.desc()).first()
+                t = {
+                    'task': task,
+                    'responses': response
+                }
+                tasks.append(t)
+            d['tasks'] = tasks
+            data.append(d)
+        print data
+        return render_template('studentGradebook.html',
+                               courses=current_user.courses,
+                               tasks=data)

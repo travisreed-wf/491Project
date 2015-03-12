116134e0b64c209dee785f1b4521bef31b18a018
Travis Reed <travis.reed@webfilings.com>
2015-03-12 07:35:20 -0500

    set image height

--- src/templates/tasks/tasks_js.js
+++ src/templates/tasks/tasks_js.js
@@ -162,7 +162,7 @@
             var src = $(f).find('input').val();
             var src = src.split("\\")[2]
             src = "/static/uploads/" + src;
-            $(f).closest('div.wp-image').find('img').first().attr("src", src);
+            $(f).closest('div.wp-image').find('img').attr("src", src);
           },
       });
   };

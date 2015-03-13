e4eb392cef0b0a635fac51fc230647a3ba89e931
Travis Reed <travis.reed@webfilings.com>
2015-03-12 23:24:46 -0500

    update authorgradebookview

--- src/grading/grading.py
+++ src/grading/grading.py
@@ -21,7 +21,7 @@ class Grader:
             correct += 1 if question.get('correct') else 0
             total_graded += 1 if question.get('correct') is not None else 0
         task_response.graded = (total == total_graded)
-        return int(float(100 * correct) / total_graded) if total else 0
+        return int(float(100 * correct) / total_graded) if total_graded else 0
 
     def grade_manual_questions(self, response_id, question_id, correct):
         response = models.TaskResponse.query.filter_by(id=response_id).first()

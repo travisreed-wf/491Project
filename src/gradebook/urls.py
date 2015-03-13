5171be268e94b3fd856db72b34e0147869dbecee
Lucas Rohret <lucasrohret@gmail.com>
2015-03-12 21:03:18 -0500

    Author gradebook can handle more than one task and student

--- src/gradebook/urls.py
+++ src/gradebook/urls.py
@@ -5,6 +5,6 @@ import views
 def setup_urls(app):
     """URLs for the gradebook functions"""
 
-<<<<<<< HEAD
-    app.add_url_rule('/gradebook', view_func=views.gradebookScreenView.as_view('Gradebook'))
+
+    app.add_url_rule('/gradebook', view_func=views.GradebookScreenView.as_view('Gradebook'))
     app.add_url_rule('/gradebook/courseGrades/<courseID>', view_func=views.courseGradeView.as_view('author_grades'))

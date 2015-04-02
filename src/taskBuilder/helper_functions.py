163557a8bb85f4e002a0f085c5d8205c0ada623c
Andrew Guibert <andy.guibert@gmail.com>
2015-04-02 15:59:25 -0500

    problem statement element + show upload filenames

--- src/taskBuilder/helper_functions.py
+++ src/taskBuilder/helper_functions.py
@@ -7,5 +7,6 @@ def get_elements():
     trueFalse = resources.Element("True or False", "elements/trueFalse", "../static/images/true-false.png")
     supplementary = resources.Element("Supp. Material", "elements/supplementary", "../static/images/video-button.png")
     textContent = resources.Element("Text Content", "elements/textContent", "../static/images/text-content.jpg")
-    elements = [multipleChoice, freeResponse, trueFalse, supplementary, textContent]
+    problemStatement = resources.Element("Problem Statement", "elements/problemStatement", "../static/images/problem-statement.png")
+    elements = [problemStatement, multipleChoice, freeResponse, trueFalse, supplementary, textContent]
     return elements

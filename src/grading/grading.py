c6bab1fc2c09c3daca9043cddb73a92bb22159a2
Andrew Hartman <ajh693@gmail.com>
2015-04-14 20:49:23 -0500

    417 stuffffff

--- src/grading/grading.py
+++ src/grading/grading.py
@@ -15,27 +15,25 @@ class Grader:
         total_graded = 0
         total = len(response['manual_questions']) + len(response['automatic_questions'])
         for question in response['automatic_questions']:
-            correct += 100 if question['correct'] else 0
+            correct += 1 if question['correct'] else 0
             total_graded += 1
         for question in response['manual_questions']:
-            correct += int(question['correctness']) if int(question['correctness']) >= 0 else 0
-            total_graded += 1 if int(question.get('correctness')) >= 0 else 0
+            correct += 1 if question.get('correct') else 0
+            total_graded += 1 if question.get('correct') is not None else 0
         task_response.graded = (total == total_graded)
-        return int(float(correct) / total_graded) if total_graded else 0
+        return int(float(100 * correct) / total_graded) if total_graded else 0
 
-    def grade_manual_question(self, response_id, question_id, correct,
-                              category='correctness'):
+    def grade_manual_questions(self, response_id, question_id, correct):
         response = models.TaskResponse.query.filter_by(id=response_id).first()
         graded_response = json.loads(response.graded_response)
         for question in graded_response['manual_questions']:
             if question['questionID'] == question_id:
-                question[category] = correct
+                question['correct'] = correct
         response.graded_response = json.dumps(graded_response)
-        if category == 'correctness':
-            correctness_grade = self.calculate_correctness(response_id)
-            response.correctness_grade = correctness_grade
-            models.db.session.commit()
-            return correctness_grade
+        correctness_grade = self.calculate_correctness(response_id)
+        response.correctness_grade = correctness_grade
+        models.db.session.commit()
+        return correctness_grade
 
     def grade_automatic_questions(self, response_id):
         task_response = models.TaskResponse.query.filter_by(id=response_id).first()
---
+++ src/grading/test_grading.py
@@ -0,0 +1,88 @@
+import json
+import unittest
+
+from mock import Mock
+from mock import patch
+
+import grading
+
+class TestGradeAutomaticQuestions(unittest.TestCase):
+
+    def setUp(self):
+        models = patch.object(grading, "models")
+        self.addCleanup(models.stop)
+        self.models = models.start()
+
+    def test_ABD(self):
+        grader = grading.Grader()
+        taskResponse = Mock()
+        taskResponse.graded_response = True
+        self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
+        ret = grader.grade_automatic_questions(1)
+        self.assertEqual(ret, None)
+
+    def test_ABCEFMPSUV(self):
+        grader = grading.Grader()
+        taskResponse = Mock()
+        taskResponse.graded_response = False
+        taskResponse.response = json.dumps({
+            'automatic_questions':[],
+            'manual_questions':[]
+            })
+        taskResponse.correctness_grade = None
+        taskResponse.task = Mock()
+        taskResponse.task.questions = json.dumps([])
+        self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
+        ret = grader.grade_automatic_questions(1)
+        self.assertEqual(taskResponse.correctness_grade, 0)
+        self.assertEqual(ret,[])
+
+class TestCalculate_Correctness(unittest.TestCase):
+
+        def setUp(self):
+            models = patch.object(grading, "models")
+            self.addCleanup(models.stop)
+            self.models = models.start()
+
+        def test_ABCDEGBCDFGBHPRS(self):
+            grader = grading.Grader()
+            taskResponse = Mock()
+            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
+            taskResponse.graded_response = json.dumps({
+            'automatic_questions':[{'correct': True},{'correct':False}],
+            'manual_questions':[]
+            })
+            taskResponse.graded = None
+            ret = grader.calculate_correctness(1)
+            self.assertEqual(taskResponse.graded, True)
+            self.assertEqual(ret,50)
+
+        def test_ABHIJKMOHPRS(self):
+            grader = grading.Grader()
+            taskResponse = Mock()
+            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
+            taskResponse.graded_response = json.dumps({
+            'automatic_questions':[],
+            'manual_questions':[{'correct':False}]
+            })
+            taskResponse.graded = None
+            ret = grader.calculate_correctness(1)
+            self.assertEqual(taskResponse.graded, True)
+            self.assertEqual(ret,0)
+
+        def test_ABHIJKMNHIJKMNHPQS(self):
+            grader = grading.Grader()
+            taskResponse = Mock()
+            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
+            taskResponse.graded_response = json.dumps({
+            'automatic_questions':[],
+            'manual_questions':[{},{}]
+            })
+            taskResponse.graded = None
+            ret = grader.calculate_correctness(1)
+            self.assertEqual(taskResponse.graded, False)
+            self.assertEqual(ret,0)
+
+
+
+

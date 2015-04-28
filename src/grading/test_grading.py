06ed12ce656cf2bef23a8f41be50cf2e09bc9280
Andrew Hartman <ajh693@gmail.com>
2015-04-28 15:20:52 -0500

    New charactereist si stuff

--- src/grading/test_grading.py
+++ src/grading/test_grading.py
@@ -96,270 +96,32 @@ class TestCalculate_Correctness(unittest.TestCase):
             self.assertEqual(taskResponse.graded, True)
             self.assertEqual(ret,100)
 
-class TestCalcualate_Correctness_Input_Characteristics(unittest.TestCase):
+class TestCalcualateCorrectnessInputCharacteristics(unittest.TestCase):
 
         def setUp(self):
             models = patch.object(grading, "models")
             self.addCleanup(models.stop)
             self.models = models.start()
 
-        def test1ValidId(self):
-            # questions answered correctly  = true
-            # percent graded  = 0
-            # number of questions = 0
-            # reponse id is valid = true
-            # question type = manual
+        def test1(self):
+            # Response id valid  = true
+            # Q1 type = manual
+            # Q1 correct = true
+            # Q2 type = manual
+            # Q2 correct = true
             grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-            'automatic_questions':[],
-            'manual_questions':[]
+            task_response = Mock()
+            self.models.TaskResponse.query.filter_by.return_value.first.return_value = task_response
+            question1 = {"correct": True}
+            question2 = {"correct": True}
+            task_response.graded_response = json.dumps({
+                'automatic_questions':[],
+                'manual_questions':[question1, question2]
             })
-            taskResponse.graded = None
+            task_response.graded = None
             ret = grader.calculate_correctness(1)
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,0)
-
-        def test2InvalidIdInfeasible(self):
-            # questions answered correctly  = true
-            # percent graded  = 1-99
-            # number of questions = multiple
-            # reponse id is valid = false
-            # question type = automatic
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[{'correct':True},{'correct':False}],
-                'manual_questions':[]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-            # length of questions = 2
-            # total graded = 2
-            # 0 = 0 so true.
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,50)
-
-        def test3InvalidIdInfeasible(self):
-            # questions answered correctly  = true
-            # percent graded  = 100
-            # number of questions = 1
-            # reponse id is valid = false
-            # question type = manual
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[],
-                'manual_questions':[{'correct': True}]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-            # length of questions = 0
-            # total graded = 0
-            # 0 = 0 so true.
-            self.assertEqual(taskResponse.graded, True)
+            self.assertEqual(task_response.graded, True)
             self.assertEqual(ret,100)
-
-        def test4InvalidIdInfeasible(self):
-            # questions answered correctly  = false
-            # percent graded  = 0
-            # number of questions = multiple
-            # reponse id is valid = false
-            # question type = automatic
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[{'correct': False},{'correct': False}],
-                'manual_questions':[]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-            # length of questions = 0
-            # total graded = 0
-            # 0 = 0 so true.
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,0)
-
-            #infeasible - Can't have 1 question answered correctly with automatic grdaed question
-            #with percent graded as 1-99
-        def test5ValidId(self):
-            # questions answered correctly  = false
-            # percent graded  = 1-99
-            # number of questions = 1
-            # reponse id is valid = true
-            # question type = automatic
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[{'correct':False}],
-                'manual_questions':[]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,0)
-
-        def test6InvalidIdInfeasible(self):
-            # questions answered correctly  = false
-            # percent graded  = 100
-            # number of questions = 0
-            # reponse id is valid = false
-            # question type = automatic
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[],
-                'manual_questions':[]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-            # length of questions = 0
-            # total graded = 0
-            # 0 = 0 so true.
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,0)
-
-        def test7InvalidIdInfeasible(self):
-            # questions answered correctly  = Not graded
-            # percent graded  = 0
-            # number of questions = 1
-            # reponse id is valid = false
-            # question type = automatic
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[{'correct':''}],
-                'manual_questions':[]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,0)
-
-            #infeasible. Can't have zero questions and a grade 1-99
-        def test8ValidId(self):
-            # questions answered correctly  = Not Graded
-            # percent graded  = 1-99
-            # number of questions = 0
-            # reponse id is valid = true
-            # question type = manual
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[],
-                'manual_questions':[]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-            # length of questions = 0
-            # total graded = 0
-            # 0 = 0 so true.
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,0)
-
-        def test9ValidIdInfeasible(self):
-            # questions answered correctly  = Not Graded
-            # percent graded  = 100
-            # number of questions = multiple
-            # reponse id is valid = true
-            # question type = manual
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[],
-                'manual_questions':[{'correct':''},{'correct':''}]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-            # length of questions = 0
-            # total graded = 0
-            # 0 = 0 so true.
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,0)
-
-        def test10InvalidIdInfeasible(self):
-            # questions answered correctly  = Not Answered
-            # percent graded  = 0
-            # number of questions = multiple
-            # reponse id is valid = false
-            # question type = automatic
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[{'correct':False},{'correct':False}],
-                'manual_questions':[]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,0)
-
-            #Infeasible. If the questions aren't answered then the score will be 0
-            # not 1-99
-        def test11ValidId(self):
-            # questions answered correctly  = Not Answered
-            # percent graded  = 1-99
-            # number of questions = 1
-            # reponse id is valid = true
-            # question type = manual
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[],
-                'manual_questions':[{'correct':False}]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,0)
-
-        def test12InvalidIdInfeasible(self):
-            # questions answered correctly  = Not Answered
-            # percent graded  = 100
-            # number of questions = 0
-            # reponse id is valid = false
-            # question type = automatic
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[],
-                'manual_questions':[]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,0)
-
-        def test13InvalidIdInfeasible(self):
-            # questions answered correctly  = false
-            # percent graded  = 0
-            # number of questions = 1
-            # reponse id is valid = false
-            # question type = manual
-            grader = grading.Grader()
-            taskResponse = Mock()
-            self.models.TaskResponse.query.filter_by.return_value.first.return_value = taskResponse
-            taskResponse.graded_response = json.dumps({
-                'automatic_questions':[],
-                'manual_questions':[{'correct':False}]
-                })
-            taskResponse.graded = None
-            ret = grader.calculate_correctness(1)
-
-            self.assertEqual(taskResponse.graded, True)
-            self.assertEqual(ret,0)
 
 
 
